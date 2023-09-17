1c98e2e5257709ba055fdd4be659bc6feaf7c912
harideep22 <61447736+harideep22@users.noreply.github.com>
2023-09-17 15:05:48 +0530

    da

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VIEWCHILD';
+
+  @ViewChild('dateIn') dateOfBirth: ElementRef | undefined;
+  @ViewChild('ageIn') age: ElementRef | undefined;
+
+  calculateAge() {
+    let birthYear = new Date(this.dateOfBirth?.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    if (this.age != undefined) {
+      this.age.nativeElement.value = age;
+    }
+    // console.log(this.dateOfBirth);
+    // console.log(this.age);
+
+  }
 }
